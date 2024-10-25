a240e0cb765ea331cd875adacab6c21f7add5573
Jacob Cash <eukelar@outlook.com>
2024-10-25 12:59:45 -0500

    refined code improved functionality

--- main.js
+++ main.js
@@ -1,23 +1,60 @@
 let display  = document.getElementsByClassName("display")[0];
-
+let operations = []
 function equals() {
-if (display.innerHTML.includes('+')) {
-let nums = display.innerHTML.split('+')
-display.innerHTML = +nums[0] + +nums[1]
-} else if (display.innerHTML.includes('-')) {
-    let nums = display.innerHTML.split('-')
-    display.innerHTML = +nums[0] - +nums[1]
-    } else if (display.innerHTML.includes('÷')) {
-        let nums = display.innerHTML.split('÷')
-        display.innerHTML = +nums[0] / +nums[1]
-        } else if (display.innerHTML.includes('x')) {
-            let nums = display.innerHTML.split('x')
-            display.innerHTML = +nums[0] * +nums[1]
-            } 
 
-            function onClick() {
-                console.log('clicked ')
-            }
+
+  
+
+
+let nums = display.innerHTML.split(/['\-'+''x''÷']/)
+let answer;
+let sums = +nums[0];
+console.log(operations)
+
+    for (let i = 1; i <= nums.length; i++) {
+       if (operations[0] === '+') {
+        sums += +nums[i]
+        operations.shift()
+
+             
+        console.log(operations)
+        console.log(nums)
+        console.log(sums)
+       } else if (operations[0] === '-') {
+        sums -= +nums[i]
+        operations.shift()
+             
+        console.log(operations)
+        console.log(nums)
+        console.log(sums)
+       } else if (operations[0] === 'x') {
+        sums *= +nums[i]
+        operations.shift()
+             
+        console.log(operations)
+        console.log(nums)
+        console.log(sums)
+       } else if (operations[0] === '÷') {
+        sums /= +nums[i]
+        operations.shift()
+             
+        console.log(operations)
+        console.log(nums)
+        console.log(sums)
+       } 
+    
+    }
+
+   
+    display.innerHTML = sums
+  
+       
+
+    
+
+console.log(sums)
+
+           
 }
           
     const buttons = document.querySelectorAll(".button")
@@ -25,18 +62,55 @@ display.innerHTML = +nums[0] + +nums[1]
 
 buttons.forEach(function(button) {
     button.addEventListener('click' ,function(){
-        display.innerHTML += this.innerHTML
+        display.innerHTML +=  this.innerHTML
+
+        if (this.innerHTML.includes('+')) {
+            if (operations.length >= 1 ) {
+                equals()
+                operations.push('+')
+                display.innerHTML += " +"
+            } else {
+            operations.push("+")
+        }
+        } else if (this.innerHTML.includes('-')) {
+            if (operations.length >= 1 ) {
+                equals()
+                operations.push('-')
+                display.innerHTML += ' -'
+            } else {  operations.push("-")
+            }
+        } else if (this.innerHTML.includes('x')) {
+            if (operations.length >= 1 ) {
+                equals()
+                operations.push('x')
+                display.innerHTML += ' x'
+            } else {operations.push('x')
+            }
+        } else if (this.innerHTML.includes("÷")) {
+            if (operations.length >= 1 ) {
+                equals()
+                operations.push('÷')
+                display.innerHTML += ' ÷'
+            } else {operations.push("÷")
+            }
+        }
     })
 })    
     
 const equalsButton = document.getElementById('equalsbutton')
 
-equalsButton.addEventListener('click', equals)
+equalsButton.addEventListener('click', function() {
+    if (operations.length > 0) {
+        equals()
+    }
+})
 
 const clearButton = document.getElementById('clear')
 
 clearButton.addEventListener('click', function() {
     display.innerHTML = ''
+    operations = []
+    
 })
 
 
